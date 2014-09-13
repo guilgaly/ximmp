482296bffe795abefe8508f1a99328be5ca8dc7d
Guillaume Galy <guilgaly@gmail.com>
2014-09-13 21:26:05 +0200

    Some basic layout work done

---
+++ ximmp-controls/src/main/java/customjavafx/guilgaly/ximmp/MediaControlBar.java
@@ -0,0 +1,8 @@
+package customjavafx.guilgaly.ximmp;
+
+public class MediaControlBar extends UserControl {
+
+    public MediaControlBar() {
+
+    }
+}
---
+++ ximmp-gui/src/main/scala/guilgaly/ximmp/gui/XimmpConf.scala
@@ -0,0 +1,8 @@
+package guilgaly.ximmp.gui
+
+import com.escalatesoft.subcut.inject.NewBindingModule
+
+class XimmpConf extends NewBindingModule(module => {
+  import module._
+
+})
